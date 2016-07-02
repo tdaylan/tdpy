1b12eb68fe031ea5cc4f2c9ff20769f8ee80af96
Tansu Daylan <tansu.daylan@gmail.com>
2016-07-02 11:54:43 -0400

    NA

--- util.py
+++ util.py
@@ -828,3 +828,26 @@ def plot_fermsmth():
     plot_heal(path, mapssmthgaus, minmlgal=minmlgal, maxmlgal=maxmlgal, minmbgal=minmbgal, maxmbgal=maxmbgal)
 
 
+def prca(matr):
+    M = (matr - mean(matr.T, 1)).T
+    eigl, eigt = linalg.eig(cov(M))
+    tranmatr = dot(eigt.T, M).T
+    return eigl, tranmatr, eigt
+
+
+def test_prca():
+
+    npara = 2
+    nsamp = 1000
+    matr = zeros((nsamp, 2))
+    matr[:, 0] = randn(nsamp)
+    matr[:, 1] = randn(nsamp) * 10.
+    eigl, tranmatr, eigt = prca(matr)
+    
+    plt.scatter(matr[:, 0], matr[:, 1])
+    plt.show()
+    
+    plt.scatter(tranmatr[:, 0], tranmatr[:, 1])
+    plt.show()
+
+
