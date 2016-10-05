224bbadff4bfe0043770e2ea435b15eefaf62e40
Tansu Daylan <tansu.daylan@gmail.com>
2016-10-05 02:45:57 -0400

    NA

--- mcmc.py
+++ mcmc.py
@@ -539,75 +539,49 @@ def retr_atcr_neww(listsamp):
 
 
 def retr_timeatcr(x, low=1, high=None, step=1, c=10, full_output=False, verbtype=1, axis=0, fast=False, boolmean=True):
-    """Estimate the integrated autocorrelation time of a time series.
-    This estimate uses the iterative procedure described on page 16 of `Sokal's
-    notes <http://www.stat.unc.edu/faculty/cji/Sokal.pdf>`_ to determine a
-    reasonable window size.
-    Args:
-        x: The time series. If multidimensional, set the time axis using the
-            ``axis`` keyword argument and the function will be computed for
-            every other axis.
-        low (Optional[int]): The minimum window size to test. (default: ``10``)
-        high (Optional[int]): The maximum window size to test. (default:
-            ``x.shape[axis] / (2*c)``)
-        step (Optional[int]): The step size for the window search. (default:
-            ``1``)
-        c (Optional[float]): The minimum number of autocorrelation times
-            needed to trust the estimate. (default: ``10``)
-        full_output (Optional[bool]): Return the final window size as well as
-            the autocorrelation time. (default: ``False``)
-        axis (Optional[int]): The time axis of ``x``. Assumed to be the first
-            axis if not specified.
-        fast (Optional[bool]): If ``True``, only use the first ``2^n`` (for
-            the largest power) entries for efficiency. (default: False)
-    Returns:
-        float or array: An estimate of the integrated autocorrelation time of
-            the time series ``x`` computed along the axis ``axis``.
-        Optional[int]: The final window size that was used. Only returned if
-            ``full_output`` is ``True``.
-    Raises
-        AutocorrError: If the autocorrelation time can't be reliably estimated
-            from the chain. This normally means that the chain is too short.
-    """
     size = 0.5 * x.shape[axis]
 
     # Compute the autocorrelation function.
     f = function(x, axis=axis, fast=fast)
 
-    # Check the dimensions of the array.
-    oned = len(f.shape) == 1
-    m = [slice(None), ] * len(f.shape)
-
-    # Loop over proposed window sizes until convergence is reached.
-    if high is None:
-        high = int(size / c)
-    for M in arange(low, high, step).astype(int):
-        # Compute the autocorrelation time with the given window.
-        if oned:
-            # Special case 1D for simplicity.
-            tau = 1 + 2 * sum(f[1:M])
-        else:
-            # N-dimensional case.
-            m[axis] = slice(1, M)
-            tau = 1 + 2 * sum(f[m], axis=axis)
-
-        # Accept the window size if it satisfies the convergence criterion.
-        if all(tau > 1.0) and M > c * tau.max():
-            if boolmean:
-                return mean(mean(f, 1), 1), mean(tau)
-            else:
-                return f, tau
-
-        # If the autocorrelation time is too long to be estimated reliably
-        # from the chain, it should fail.
-        if c * tau.max() >= size:
-            break
+    if False:
+        # Check the dimensions of the array.
+        oned = len(f.shape) == 1
+        m = [slice(None), ] * len(f.shape)
     
-    print 'Autocorrelation time could not be estimated'
-    if boolmean:
-        return mean(mean(f, 1), 1), 0.
+        # Loop over proposed window sizes until convergence is reached.
+        if high is None:
+            high = int(size / c)
+        for M in arange(low, high, step).astype(int):
+            # Compute the autocorrelation time with the given window.
+            if oned:
+                # Special case 1D for simplicity.
+                tau = 1 + 2 * sum(f[1:M])
+            else:
+                # N-dimensional case.
+                m[axis] = slice(1, M)
+                tau = 1 + 2 * sum(f[m], axis=axis)
+    
+            # Accept the window size if it satisfies the convergence criterion.
+            if all(tau > 1.0) and M > c * tau.max():
+                if boolmean:
+                    return mean(mean(f, 1), 1), mean(tau)
+                else:
+                    return f, tau
+    
+            # If the autocorrelation time is too long to be estimated reliably
+            # from the chain, it should fail.
+            if c * tau.max() >= size:
+                break
+        
+        print 'Autocorrelation time could not be estimated'
+        if boolmean:
+            return mean(mean(f, 1), 1), 0.
+        else:
+            return f, zeros(x.shape[1:])
     else:
-        return f, zeros(x.shape[1:])
+        timeatcr = amax(where(f > 0.2)[0])
+        return mean(mean(f, 1), 1), timeatcr
 
 
 def function(x, axis=0, fast=False):
@@ -903,9 +877,15 @@ def plot_grid(path, listsamp, strgpara, lims=None, scalpara=None, plotsize=6, nu
                     axis.set_xticklabels(strgarry)
                 axis.set_xlim(thislims[:, l])
                 if k == thisnumbpara - 1:
-                    axis.set_xlabel(thisparastrg[l])  
+                    axis.set_xlabel(thisparastrg[l])
+                else:
+                    axis.set_xticklabels([])
                 if l == 0 and k != 0:
                     axis.set_ylabel(thisparastrg[k])
+                else:
+                    if k != 0:
+                        axis.set_yticklabels([])
+
         figr.subplots_adjust(bottom=0.2)
         strg = 'grid'
         if numbfram != 1:
