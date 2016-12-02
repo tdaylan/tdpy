abdcc57bc804e5afccee307d88895abdfd171937
Tansu Daylan <tansu.daylan@gmail.com>
2016-12-02 09:30:48 -0600

    NA

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from distutils.core import setup
 
-setup(name='tdpy', \
-      version='1.0', \
-      package_dir={'tdpy': ''},
-      packages=['tdpy'],
+setup( \
+      name='tdpy', \
+      packages=['tdpy'], \
+      version='0.1', \
+      description='A library of MCMC and numerical routines', \
+      author='Tansu Daylan', \
+      author_email='tansu.daylan@gmail.com', \
+      url='https://github.com/tdaylan/tdpy', \
+      download_url='https://github.com/tdaylan/tdpy/tarball/v0.1-alpha', \
+      keywords=['mcmc', 'sampling', 'numerical'], \
      )
 
