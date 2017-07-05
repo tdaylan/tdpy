befff9bbbab09cb9ed99263c42f56f37bcd21011
Tansu Daylan <tansu.daylan@gmail.com>
2017-07-05 08:09:31 -0400

    NA

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ from __init__ import *
 from tdpy import mcmc
 from tdpy import util
 
-def retr_llik_flag(sampvarb):
+def retr_llik_flag(sampvarb, gdatextr):
         
     xpos = sampvarb[0]
     ypos = sampvarb[1]
-
-    llik = sp.interpolate.interp2d(xgrd, ygrd, pdfn)(xpos, ypos)
+    
+    llik = sp.interpolate.interp2d(gdatextr.xgrd, gdatextr.ygrd, gdatextr.pdfn)(xpos, ypos)
     sampcalc = [arange(5), arange(6)]
 
     return llik, sampcalc
@@ -16,7 +16,19 @@ def retr_llik_flag(sampvarb):
 def retr_datapara():
     
     numbpara = 2
-    
+
+    datapara = util.gdatstrt()
+    datapara.indx = dict()
+    datapara.minm = zeros(numbpara)
+    datapara.maxm = zeros(numbpara)
+    datapara.name = empty(numbpara, dtype=object)
+    datapara.scal = empty(numbpara, dtype=object)
+    datapara.labl = empty(numbpara, dtype=object)
+    datapara.unit = empty(numbpara, dtype=object)
+    datapara.true = empty(numbpara, dtype=object)
+    datapara.vari = zeros(numbpara)
+
+
     datapara.indx['xpos'] = 0
     datapara.indx['ypos'] = 0
     datapara.name[0] = 'xpos'
@@ -30,63 +42,68 @@ def retr_datapara():
     datapara.vari[:] = 1e-1
     datapara.true[:] = None
     datapara.strg = datapara.labl + ' ' + datapara.unit
+    
+    return datapara
 
 
-def cnfg_flag():
+def cnfg_flag(flagtype='turk'):
+
+    from scipy import ndimage
+    gdat = util.gdatstrt()
+    pathimag = os.environ["TDPY_DATA_PATH"] + '/imag/'
+    pathimagrtag = pathimag + flagtype + 'flag/'
+    path = os.environ["TDPY_DATA_PATH"] + '/data/%sflag.png' % flagtype
 
     # target PDF
-    path = path = os.environ["TDPY_DATA_PATH"] + '/turkflag.png'
-    imag = sp.ndimage.imread(path)
+    imag = flipud(sp.ndimage.imread(path))
     rati = float(imag.shape[0]) / imag.shape[1]
     xinp = linspace(0., 1., imag.shape[1])
     yinp = linspace(0., 1., imag.shape[0])
     numbxgrd = 200
     numbygrd = int(200 * rati)
-    xgrd = linspace(0., 1., numbxgrd)
-    ygrd = linspace(0., 1., numbygrd)
-    imag = sp.ndimage.gaussian_filter(imag, sigma=[xinp.size / 100, yinp.size / 100, 0])
-    pdfn = zeros((numbygrd, numbxgrd, 3))
+    gdat.xgrd = linspace(0., 1., numbxgrd)
+    gdat.ygrd = linspace(0., 1., numbygrd)
+    if False:
+        imag = sp.ndimage.gaussian_filter(imag, sigma=[xinp.size / 300, yinp.size / 300, 0])
+    gdat.pdfn = zeros((numbygrd, numbxgrd, 3))
     for k in range(3):
-        pdfn[:, :, k] = sp.interpolate.interp2d(xinp, yinp, imag[:, :, k])(xgrd, ygrd)
-    pdfn = 0.3 * pdfn[:, :, 0]
+        gdat.pdfn[:, :, k] = sp.interpolate.interp2d(xinp, yinp, imag[:, :, k])(gdat.xgrd, gdat.ygrd)
+    gdat.pdfn = 0.3 * gdat.pdfn[:, :, 0]
     
     figr, axis = plt.subplots()
     axis.imshow(imag, extent=[0., 1., 0., 1.], interpolation='none', aspect=rati)
-    plt.savefig('imag.pdf')
+    plt.savefig(pathimagrtag + 'imag.pdf')
     plt.close(figr) 
     
     figr, axis = plt.subplots()
-    imag = axis.imshow(pdfn, extent=[0., 1., 0., 1.], interpolation='none', aspect=rati)
+    imag = axis.imshow(gdat.pdfn, extent=[0., 1., 0., 1.], interpolation='none', aspect=rati)
     plt.colorbar(imag)
-    plt.savefig('targ.pdf')
+    plt.savefig(pathimagrtag + 'targ.pdf')
     plt.close(figr) 
     
     # MCMC setup
     verbtype = 1
-    numbproc = 2
-    numbswep = 7
     datapara = retr_datapara()
     optiprop = True
-    pathbase = '/Users/tansu/Desktop/'
     
     # run MCMC
-    sampbund = mcmc.init(numbproc, numbswep, retr_llik_flag, datapara, pathbase=pathbase, optiprop=True, verbtype=verbtype, rtag='flag')
+    sampbund = mcmc.init(retr_llik_flag, datapara, numbswep=10000000, factthin=100, gdatextr=gdat, pathimag=pathimagrtag, optiprop=True, verbtype=verbtype, rtag='flag')
     listxpos = sampbund[0][:, 0]
     listypos = sampbund[0][:, 1]
     numbsamp = listxpos.size
-    pathtemp = pathbase + 'post'
-    numbfram = 10
+    numbfram = 100
     for k in range(numbfram):
         indxsamp = int(numbsamp * float(k) / numbfram)
         figr, axis = plt.subplots()
         axis.scatter(listxpos[:indxsamp], listypos[:indxsamp], s=3)
+
         axis.set_xlim([0., 1.])
         axis.set_ylim([0., 1.])
-        path = pathtemp + '%04d.pdf' % k
+        path = pathimagrtag + 'thisimag%04d.pdf' % k
         plt.savefig(path)
         plt.close(figr) 
     
-    cmnd = 'convert -delay 20 %s*.pdf post.gif' % pathtemp
+    cmnd = 'convert -delay 20 %sthis*.pdf %spostimag.gif' % (pathimagrtag, pathimagrtag)
     os.system(cmnd)
 
 
@@ -143,5 +160,6 @@ def cnfg_gaus():
     # run MCMC
     sampbund = mcmc.init(retr_llik_ebox, datapara, numbswep=100000, optiprop=True, gdatextr=gdat, numbburn=0, factthin=10, rtag='gausgame', numbbinsplot=100, fracrand=0.1)
 
-cnfg_gaus()
+
+globals().get(sys.argv[1])(*sys.argv[2:])
 
